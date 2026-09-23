44a4e6c99470557dcc39f325dd8d9de26c5b8e58
Rupash Das <rupash.das.40@gmail.com>
2026-09-24 03:35:09 +0600

    User factory updated

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -10,8 +10,7 @@ use Illuminate\Support\Str;
 /**
  * @extends Factory<User>
  */
-class UserFactory extends Factory
-{
+class UserFactory extends Factory {
     /**
      * The current password being used by the factory.
      */
@@ -22,8 +21,7 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
+    public function definition(): array {
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
@@ -36,14 +34,13 @@ class UserFactory extends Factory
     /**
      * Indicate that the model's email address should be unverified.
      */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
+    public function unverified(): static {
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
 
     public function inactive(): static {
-        return $this->state(fn (array $attributes) => ['is_active' => false]);
+        return $this->state(fn(array $attributes) => ['is_active' => false]);
     }
 }
